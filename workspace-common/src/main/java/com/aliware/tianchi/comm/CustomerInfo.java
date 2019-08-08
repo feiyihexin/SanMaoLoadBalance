a30cdd0d13920e099eb134b2507c9c1f02b19241
hexinxin <49236454+feiyihexin@users.noreply.github.com>
2019-08-08 21:10:20 +0800

    Update CustomerInfo.java

--- workspace-common/src/main/java/com/aliware/tianchi/comm/CustomerInfo.java
+++ workspace-common/src/main/java/com/aliware/tianchi/comm/CustomerInfo.java
@@ -22,11 +22,11 @@ public class CustomerInfo {
         this.env = env;
         this.providerAllThreads = (int) (providerThread * 0.9);
         if ("large".equals(env)) {
-            this.serverWeight = 15;
+            this.serverWeight = 27;
         } else if ("medium".equals(env)) {
             this.serverWeight = 9;
         } else if ("small".equals(env)) {
-            this.serverWeight = 2;
+            this.serverWeight = 3;
         } else {
             this.serverWeight = 1;
         }
